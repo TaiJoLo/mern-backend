b659d980a7e54c64190820f960bbeb218a1dd840
LOLO <lotaijo@gmail.com>
2023-03-14 06:09:19 +1300

    adding place-specific routes

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,10 +2,43 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log("GET request in Places");
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://images.unsplash.com/photo-1620400557579-93560a77987c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1365&q=80",
+    address: "20 W 34th St., New York, NY 10001, United States",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878531,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building2",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://images.unsplash.com/photo-1620400557579-93560a77987c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1365&q=80",
+    address: "20 W 34th St., New York, NY 10001, United States",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878531,
+    },
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
   res.json({
-    message: "it works!",
+    place: place,
   });
 });
 
