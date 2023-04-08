712e7b80f7ac66806c8656b43ef0d674d8962869
LOLO <lotaijo@gmail.com>
2023-04-08 18:48:29 +1200

    fixing api keys

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `${process.env.MONGODB_URI}`,
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.s3klmsv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
 
     {
       useNewUrlParser: true,
--- dotenv.js
+++ dotenv.js
@@ -1,8 +1,11 @@
-console.log("No value for API_KEY yet:", process.env.API_KEY);
+console.log("No value for GOOGLE_API_KEY yet:", process.env.GOOGLE_API_KEY);
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
-console.log("Now the value for API_KEY is:", process.env.API_KEY);
-console.log("Now connecting to MongoDB", process.env.MONGODB_URI);
+console.log("Now the value for GOOGLE_API_KEY is:", process.env.GOOGLE_API_KEY);
+console.log(
+  "Now connecting to MongoDB",
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.s3klmsv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+);
