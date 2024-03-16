26f94b8c583c9045ce5091ea1cb469a7aa2d54a9
TaiJoLo <lotaijo@gmail.com>
2024-03-16 22:52:06 +1300

    refactor: deploy a combined app

---
+++ public/precache-manifest.e55b23e98fa84e0c82c9212a261f2439.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf1e5a0472535e65bf8c300bd7cc255c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "606f7c990965f59e1c99",
+    "url": "/static/css/0.9a0b5772.chunk.css"
+  },
+  {
+    "revision": "5c6d9d106910cf357530",
+    "url": "/static/css/4.1c39cc9c.chunk.css"
+  },
+  {
+    "revision": "78ed1699fedb9999a039",
+    "url": "/static/css/5.248832c2.chunk.css"
+  },
+  {
+    "revision": "f22ef4aa06839c511206",
+    "url": "/static/css/6.838ae058.chunk.css"
+  },
+  {
+    "revision": "fb2a9ffb1b5cf8e82f3b",
+    "url": "/static/css/7.47ada663.chunk.css"
+  },
+  {
+    "revision": "89f52121e0e259acfcc6",
+    "url": "/static/css/8.f0697d89.chunk.css"
+  },
+  {
+    "revision": "85b37a25647325c1a99e",
+    "url": "/static/css/main.30e84aef.chunk.css"
+  },
+  {
+    "revision": "606f7c990965f59e1c99",
+    "url": "/static/js/0.311b07c3.chunk.js"
+  },
+  {
+    "revision": "555e7111d6ba9cc97203",
+    "url": "/static/js/3.c8844c22.chunk.js"
+  },
+  {
+    "revision": "5c6d9d106910cf357530",
+    "url": "/static/js/4.6b816adb.chunk.js"
+  },
+  {
+    "revision": "78ed1699fedb9999a039",
+    "url": "/static/js/5.cb8b31f4.chunk.js"
+  },
+  {
+    "revision": "f22ef4aa06839c511206",
+    "url": "/static/js/6.d253eb96.chunk.js"
+  },
+  {
+    "revision": "fb2a9ffb1b5cf8e82f3b",
+    "url": "/static/js/7.2d6b913f.chunk.js"
+  },
+  {
+    "revision": "89f52121e0e259acfcc6",
+    "url": "/static/js/8.b8caed08.chunk.js"
+  },
+  {
+    "revision": "85b37a25647325c1a99e",
+    "url": "/static/js/main.19160163.chunk.js"
+  },
+  {
+    "revision": "a176203a19c91a55a989",
+    "url": "/static/js/runtime-main.311ab6e5.js"
+  }
+]);
